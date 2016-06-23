9f1a5822c63715d3187b1ed0fb58169cc147176c
b27cs <emilj@hotmail.com>
2016-06-22 22:54:51 -0400

    Added gestures for the Firetrails and Waterfall. Also some updates on the GravityBall gesture. Also, Fire gesture parameterization is now saved after gesturing.

--- Unity3D/InteractiveDance/Assets/Scripts/CoreScripts/GestureManager.cs
+++ Unity3D/InteractiveDance/Assets/Scripts/CoreScripts/GestureManager.cs
@@ -40,6 +40,41 @@ namespace Assets.Scripts.CoreScripts
             }
         }
 
+        public static class FireTrailPath
+        {
+            private static float _initialSize;
+            private const float StartSizeMin = 0.2f, StartSizeMax = 20f;
+            private static readonly GestureParameter StartSizeGesture = new GestureParameter(StartSizeMin, StartSizeMax);
+            public static float StartSize = 1f;
+
+
+            private static float _initialLifeTime;
+            private const float StartLifeTimeMin = 0.2f, StartLifeTimeMax = 10f;
+            private static readonly GestureParameter StartLocationGesture = new GestureParameter(StartLifeTimeMin, StartLifeTimeMax);
+            public static float StartLifeTime = 1.5f;
+
+            public static void Initialize()
+            {
+                _initialSize = StartSize;
+                _initialLifeTime = StartLifeTime;
+            }
+
+            //value here comes in from left hand y coordinate
+            public static void SetSize(float leftHandY, float rightHandY)
+            {
+                float handYDistance = Mathf.Abs((rightHandY - leftHandY));
+                StartSize = StartSizeGesture.Clamp(_initialSize + StartSizeGesture.GetUnit() * handYDistance);
+            }
+
+            //value here comes in from left hand x coordinate
+            public static void SetLifetime(float leftHandX, float rightHandX)
+            {
+                float handXDistance = Mathf.Abs((rightHandX - leftHandX));
+                StartLifeTime = StartSizeGesture.Clamp(_initialSize + StartSizeGesture.GetUnit() * handXDistance);
+            }
+        }
+
+
         public static class Piano
         {
             private static float _initialX = 1, _initialY = 1;
@@ -90,10 +125,10 @@ namespace Assets.Scripts.CoreScripts
 
             public static void SetSpeedUpFactor(float leftHandX, float leftHandY, float rightHandX, float rightHandY)
             {
-                //We will set a speed up factor by comarping the current hand positions to the previous hand positions
-                //This gives a rough estimate of the speed of movement of the player
-                float xdistance = Vector3.Distance((new Vector3(leftHandX, leftHandY, 0)), previousLeftHandPostion);
-                xdistance = SpeedGesture.Clamp(xSpeedFactor + SpeedGesture.GetUnit() * xdistance);
+                //We will set a speed up factor by comparing the current hand positions to the previous hand positions
+                //This gives a rough estimate of the speed of movement of the player's hand(and by extension their body)
+                float leftHandDistanceChange = Vector3.Distance((new Vector3(leftHandX, leftHandY, 0)), previousLeftHandPostion);
+                //leftHandDistanceChange = SpeedGesture.Clamp(xSpeedFactor + SpeedGesture.GetUnit() * leftHandDistanceChange);
 
                 float ydistance = Vector3.Distance((new Vector3(rightHandX, rightHandY, 0)), previousRightHandPostion);
                 ydistance = SpeedGesture.Clamp(xSpeedFactor + SpeedGesture.GetUnit() * ydistance);
@@ -105,8 +140,52 @@ namespace Assets.Scripts.CoreScripts
                 previousLeftHandPostion.y = leftHandY;
 
                 //Set the force vector for the ball
-                forceVector.x = xdistance;
-                forceVector.y = ydistance;
+                forceVector.x = Mathf.Abs(leftHandDistanceChange*30);
+                //forceVector.y = ydistance;
+                forceVector.y = Mathf.Abs(leftHandDistanceChange*30); 
+
+            }
+
+
+        }
+
+
+        public static class Waterfall
+        {
+            //private static Vector3 previousRightHandPostion;
+           // private static Vector3 previousLeftHandPostion;
+
+            public static Vector3 scaleVector;
+
+            private static float xSpeedFactor;
+            //private const float xSpeedMin = -50, xSpeedMax = 50;
+            //private static readonly GestureParameter ScaleGesture = new GestureParameter(xSpeedMin, xSpeedMax);
+
+            public static void Initialize()
+            {
+                scaleVector = new Vector3(1, 1, 1);
+                //previousRightHandPostion = new Vector3(0, 0, 0);
+                //previousLeftHandPostion = new Vector3(0, 0, 0);
+            }
+
+            public static void SetWaterFallScale(float leftHandX, float leftHandY, float rightHandX, float rightHandY)
+            {
+                float xHandDistance = (rightHandX - leftHandX)*(float)0.01;
+                //leftHandDistanceChange = SpeedGesture.Clamp(xSpeedFactor + SpeedGesture.GetUnit() * leftHandDistanceChange);
+
+                float yHandDistance = rightHandY - leftHandY;
+                //ydistance = SpeedGesture.Clamp(xSpeedFactor + SpeedGesture.GetUnit() * ydistance);
+
+                //Update the hand positions for the next iteration
+                //previousRightHandPostion.x = rightHandX;
+                //previousRightHandPostion.y = rightHandY;
+                //previousLeftHandPostion.x = leftHandX;
+                //previousLeftHandPostion.y = leftHandY;
+
+                //Set the scale vector for the waterfall
+                scaleVector.x = Mathf.Abs(xHandDistance);
+                //forceVector.y = ydistance;
+                //scaleVector.y = Mathf.Abs(yHandDistance);
 
             }
 
---
+++ Unity3D/InteractiveDance/Assets/Scripts/GestureScripts/FireTrail.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Assets.Scripts.CoreScripts;
+
+public class FireTrail : MonoBehaviour, IGesturable
+{
+
+    private ParticleSystem _fireTrail;
+
+    // Use this for initialization
+    void Start ()
+    {
+        GestureActivation.CurrentGesture = this;
+        _fireTrail = gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    //if (!GestureActivation.IsGesturing) return;
+
+        //If we are gesturing, the fire trail size and lifetime values will be updated.
+        //If we are not gesturing, we will be picking up the values that were set by the gesture system
+
+        _fireTrail.startLifetime = GestureManager.FireTrailPath.StartLifeTime;
+        _fireTrail.startSize = GestureManager.FireTrailPath.StartSize;
+
+    }
+
+    public void OnNext(float leftHandX, float leftHandY, float rightHandX, float rightHandY)
+    {
+        GestureManager.FireTrailPath.SetSize(leftHandY, rightHandY);
+        GestureManager.FireTrailPath.SetLifetime(leftHandX, rightHandX);
+    }
+
+    public void OnStart()
+    {
+        GestureManager.FireTrailPath.Initialize();
+    }
+    public void OnCompleted()
+    {
+
+    }
+
+
+}
